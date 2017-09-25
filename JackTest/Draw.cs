c9733a46f8229b07f5d1890acd7bf9e68ee0266f
devstanis(PC) <devstanis@gmail.com>
2017-09-25 11:50:08 +0300

    Добавил классы draw, lotto

---
+++ JackTest/Draw.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JackTest
+{
+    class Draw
+    {
+        public DateTime Date { get; private set; }
+        public int DrawNum { get; private set; }
+        public int[] Nums { get; private set; }
+        public int JackSum { get; private set; }
+        public bool IsJackWin { get; private set; }
+
+        public Draw(int _drawNum, DateTime _date, int[] _nums,int _jack, bool _isWin)
+        {
+            DrawNum = _drawNum;
+            Date = _date;
+            Array.Copy(_nums, Nums, _nums.Length);
+            JackSum = _jack;
+            IsJackWin = _isWin;
+        }
+    }
+}
---
+++ JackTest/Lotto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JackTest
+{
+    class Lotto
+    {
+        public string Name { get; private set; }
+        List<Draw> drawList;
+        public Lotto(string _name)
+        {
+            Name = _name;
+            drawList = new List<Draw>();
+        }
+
+        public void AddDraw(Draw _draw)
+        {
+            if(_draw !=null)
+                drawList.Add(_draw);
+        }
+
+        public Draw[] GetAllDraws()
+        {
+            Draw[] draws = new Draw[drawList.Count];
+            drawList.CopyTo(draws);
+            return draws;
+        }
+    }
+}
